b2ce03fc0309e0d8e523dbadde1f774bd4af29da
Mikael Lund <mlund@mac.com>
2016-05-25 11:49:33 +0200

    json example update

--- python/json-example.py
+++ python/json-example.py
@@ -1,28 +1,27 @@
-import datetime, json, numpy as np
+import datetime, json, numpy
 
 # just some data
-date = str(datetime.datetime.utcnow())
-version = 1
-revision = 2
+date = str(datetime.datetime.utcnow()) # date stamp in UCT format
+cnt_samples = 1 # current sample count
+cnt_spectra = 1 # current spectrum count
 
-# let's generate a spectrum
-l = np.arange(500,520,2.0)
-a = np.random.random(len(l))
+# let's generate an absorbance spectrum
+l = numpy.arange(500,520,2.0)
+a = numpy.random.random(len(l)).round(3)
 
 d = {
         'comment' : 'Spectacular acid titration of acetic acid...',
         'operator': 'Gus McBain',
-        'uctdate' : date,
-        'version' : version,
-        'revision' : revision,
+        'utcdate' : date,
+        'version' : [1, 5, 2],     # software version
         'samplepoints' : {
-            '0' : {
-                'uctdate' : date,
+            str(cnt_samples).zfill(4) : {
+                'utcdate' : date,
                 'electrode' : 7.1,
                 'temperature' : 298.15,
                 'volume' : 40.,
-                'absorbance_spectra' : {
-                    "0" : {
+                'absorbance_spectra' : { # each sample point can have several spectra
+                    str(cnt_spectra).zfill(2) : {
                         'wavelength' : l.tolist(),
                         'absorbance' : a.tolist()
                         }
@@ -31,5 +30,9 @@ d = {
             }
         }
 
-print json.dumps( d , indent=4, separators=(',', ': '))
+# pretty print
+print json.dumps( d , indent=4, separators=(',', ': ')) + '\n'
+
+# compact print
+print json.dumps(d)
 
